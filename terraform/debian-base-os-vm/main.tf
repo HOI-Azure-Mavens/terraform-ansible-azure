a2a208bd61d56509edaa2691773d86dc5f9dff40
M AL-SOURI <msalsouri@hotmail.com>
2024-12-13 05:25:33 +0000

    Added configuration for Debian base OS VM and tested destruction

---
+++ terraform/debian-base-os-vm/main.tf
@@ -0,0 +1,27 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "1001490f-c77c-403e-be9e-97eac578d1d6"
+}
+
+module "debian_base_vm" {
+  source              = "../modules/vm"
+  vm_name             = "debian-base-os-vm"
+  location            = "northeurope"
+  resource_group_name = "debian-base-os-vm-rg"
+  vm_size             = "Standard_D2s_v3"
+  admin_username      = "adminuser"
+  ssh_public_key_path = "~/.ssh/id_rsa.pub"
+  os_offer            = "debian-13-daily"  # Corrected Offer
+  os_sku              = "13-gen2"          # Corrected SKU
+  os_version          = "0.20241031.1917"  # Use a specific version
+}
+
+output "vm_public_ip" {
+  value = module.debian_base_vm.vm_public_ip
+}
+
+
+
+
+
+
