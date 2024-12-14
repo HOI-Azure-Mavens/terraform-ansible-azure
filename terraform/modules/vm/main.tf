d2959f14606ea457dfae63bb1104f56a2e03e84e
M AL-SOURI <msalsouri@hotmail.com>
2024-12-14 22:59:53 +0000

    Finalize Debian Base OS VM Terraform configuration and update module structure

--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -1,12 +1,60 @@
-variable "vm_name" {}
-variable "location" {}
-variable "resource_group_name" {}
-variable "vm_size" {}
-variable "admin_username" {}
-variable "ssh_public_key_path" {}
-variable "os_offer" {}
-variable "os_sku" {}
-variable "os_version" {}
+resource "azurerm_virtual_network" "vm_vnet" {
+  name                = "${var.vm_name}-vnet"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "vm_subnet" {
+  name                 = "${var.vm_name}-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vm_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "vm_nsg" {
+  name                = "${var.vm_name}-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "vm_subnet_nsg" {
+  subnet_id                 = azurerm_subnet.vm_subnet.id
+  network_security_group_id = azurerm_network_security_group.vm_nsg.id
+}
+
+resource "azurerm_public_ip" "vm_pip" {
+  name                = "${var.vm_name}-pip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "${var.vm_name}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.vm_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_pip.id
+  }
+}
 
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = var.vm_name
@@ -23,7 +71,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 
   source_image_reference {
-    publisher = "Canonical"
+    publisher = "Debian"
     offer     = var.os_offer
     sku       = var.os_sku
     version   = var.os_version
@@ -35,27 +83,4 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 }
 
-resource "azurerm_network_interface" "vm_nic" {
-  name                = "${var.vm_name}-nic"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.vm_subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
 
-resource "azurerm_subnet" "vm_subnet" {
-  name                 = "${var.vm_name}-subnet"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = "${var.vm_name}-vnet"
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_virtual_network" "vm_vnet" {
-  name                = "${var.vm_name}-vnet"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  address_space       = ["10.0.0.0/16"]
-}
--- terraform/modules/vm/outputs.tf
+++ terraform/modules/vm/outputs.tf
@@ -5,3 +5,4 @@ output "vm_name" {
 output "vm_public_ip" {
   value = azurerm_public_ip.vm_pip.ip_address
 }
+
