f17fb45789c024ecfe27834b77fbb44639e82f5c
M AL-SOURI <msalsouri@hotmail.com>
2024-12-14 03:09:07 +0000

    Add Azure SQL Database and Server configuration with Terraform

--- terraform/azure-sql-database-db/main.tf
+++ terraform/azure-sql-database-db/main.tf
@@ -3,22 +3,42 @@ provider "azurerm" {
   subscription_id = "1001490f-c77c-403e-be9e-97eac578d1d6"
 }
 
+# Define the resource group
+resource "azurerm_resource_group" "sql_rg" {
+  name     = "hoi-azure-sql-database-rg"
+  location = "northeurope"
+}
+
+# Define the MSSQL server resource
+resource "azurerm_mssql_server" "sql_server" {
+  name                         = "hoi-sql-server"
+  resource_group_name          = azurerm_resource_group.sql_rg.name
+  location                     = azurerm_resource_group.sql_rg.location
+  version                      = "12.0"
+  administrator_login          = var.admin_username
+  administrator_login_password = var.admin_password
+  minimum_tls_version         = "1.2"
+  public_network_access_enabled = true
+
+  # Remove the connection_policy setting as it's handled automatically
+  depends_on = [azurerm_resource_group.sql_rg]
+
+  timeouts {
+    create = "60m"
+    delete = "60m"
+  }
+}
+
+# Define the SQL database module
 module "sql_database" {
   source              = "../modules/sql-database"
-  sql_server_name     = var.sql_server_name
+  server_id           = azurerm_mssql_server.sql_server.id
   database_name       = var.database_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
   sku_name            = var.sku_name
   collation           = var.collation
   max_size_gb         = var.max_size_gb
   tags                = var.tags
+
+  depends_on = [azurerm_mssql_server.sql_server]
 }
-
-
-
-
-
 
--- terraform/azure-sql-database-db/outputs.tf
+++ terraform/azure-sql-database-db/outputs.tf
@@ -1,12 +1,5 @@
-output "sql_server_fqdn" {
-  value       = module.sql_database.sql_server_fqdn
-  description = "The fully qualified domain name of the SQL Server."
-}
-
 output "sql_database_id" {
-  value       = module.sql_database.sql_database_id
   description = "The ID of the SQL Database."
+  value       = module.sql_database.sql_database_id
 }
-
-
 
