24605481ca708133ad550dfca4707f32a84bc0f4
M AL-SOURI <msalsouri@hotmail.com>
2024-12-15 01:38:17 +0000

    Finalize Kali Base OS VM with key generation and cleanup

--- terraform/kali-base-os-vm/main.tf
+++ terraform/kali-base-os-vm/main.tf
@@ -3,14 +3,94 @@ provider "azurerm" {
   subscription_id = "1001490f-c77c-403e-be9e-97eac578d1d6"
 }
 
+# Generate SSH Key Pair
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Resource Group
 resource "azurerm_resource_group" "vm_rg" {
   name     = "${var.vm_name}-rg"
   location = var.location
 }
 
+# Network Security Group
+resource "azurerm_network_security_group" "vm_nsg" {
+  name                = "${var.vm_name}-nsg"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.vm_rg.name
+}
+
+# Allow SSH Rule
+resource "azurerm_network_security_rule" "allow_ssh" {
+  name                        = "AllowSSH"
+  priority                    = 1000
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_address_prefix       = "0.0.0.0/0"
+  destination_address_prefix  = "*"
+  destination_port_range      = "22"
+  source_port_range           = "*"
+  network_security_group_name = azurerm_network_security_group.vm_nsg.name
+  resource_group_name         = azurerm_resource_group.vm_rg.name
+}
+
+# Subnet to NSG Association
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg" {
+  subnet_id                 = azurerm_subnet.vm_subnet.id
+  network_security_group_id = azurerm_network_security_group.vm_nsg.id
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vm_vnet" {
+  name                = "${var.vm_name}-vnet"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.vm_rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Subnet
+resource "azurerm_subnet" "vm_subnet" {
+  name                 = "${var.vm_name}-subnet"
+  resource_group_name  = azurerm_resource_group.vm_rg.name
+  virtual_network_name = azurerm_virtual_network.vm_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Public IP
+resource "azurerm_public_ip" "vm_pip" {
+  name                = "${var.vm_name}-pip"
+  location            = azurerm_resource_group.vm_rg.location
+  resource_group_name = azurerm_resource_group.vm_rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Network Interface
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "${var.vm_name}-nic"
+  location            = azurerm_resource_group.vm_rg.location
+  resource_group_name = azurerm_resource_group.vm_rg.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.vm_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_pip.id
+  }
+}
+
+# NIC to NSG Association
+resource "azurerm_network_interface_security_group_association" "vm_nic_nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.vm_nic.id
+  network_security_group_id = azurerm_network_security_group.vm_nsg.id
+}
+
+# Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = var.vm_name
-  location            = var.location
+  location            = azurerm_resource_group.vm_rg.location
   resource_group_name = azurerm_resource_group.vm_rg.name
   size                = var.vm_size
   admin_username      = var.admin_username
@@ -37,41 +117,47 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file(var.ssh_public_key_path)
+    public_key = tls_private_key.ssh_key.public_key_openssh
   }
 }
 
-resource "azurerm_public_ip" "vm_pip" {
-  name                = "${var.vm_name}-pip"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.vm_rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+# Output Variables
+output "vm_name" {
+  value       = azurerm_linux_virtual_machine.vm.name
+  description = "The name of the virtual machine."
 }
 
-resource "azurerm_network_interface" "vm_nic" {
-  name                = "${var.vm_name}-nic"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.vm_rg.name
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.vm_subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm_pip.id
-  }
+output "vm_public_ip" {
+  value       = azurerm_public_ip.vm_pip.ip_address
+  description = "The public IP address of the virtual machine."
 }
 
-resource "azurerm_subnet" "vm_subnet" {
-  name                 = "${var.vm_name}-subnet"
-  resource_group_name  = azurerm_resource_group.vm_rg.name
-  virtual_network_name = azurerm_virtual_network.vm_vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+output "vm_private_ip" {
+  value       = azurerm_network_interface.vm_nic.private_ip_address
+  description = "The private IP address of the virtual machine."
 }
 
-resource "azurerm_virtual_network" "vm_vnet" {
-  name                = "${var.vm_name}-vnet"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.vm_rg.name
-  address_space       = ["10.0.0.0/16"]
+output "resource_group_name" {
+  value       = azurerm_resource_group.vm_rg.name
+  description = "The name of the resource group."
 }
+
+output "virtual_network_name" {
+  value       = azurerm_virtual_network.vm_vnet.name
+  description = "The name of the virtual network."
+}
+
+output "subnet_name" {
+  value       = azurerm_subnet.vm_subnet.name
+  description = "The name of the subnet."
+}
+
+output "private_key" {
+  value       = tls_private_key.ssh_key.private_key_pem
+  sensitive   = true
+  description = "The private SSH key for the VM."
+}
+
+
+
 
