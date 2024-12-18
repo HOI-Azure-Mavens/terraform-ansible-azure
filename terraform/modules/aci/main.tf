ccac5b5f689333dfbe085682b62854df5e5b46c8
M AL-SOURI <msalsouri@hotmail.com>
2024-12-18 03:57:56 +0000

    Fix formatting issues

--- terraform/modules/aci/main.tf
+++ terraform/modules/aci/main.tf
@@ -1,11 +1,11 @@
 resource "azurerm_container_group" "aci" {
   name                = var.container_group_name
-  location            = var.location              # Use variable for location
-  resource_group_name = var.resource_group_name   # Use variable for RG
+  location            = var.location            # Use variable for location
+  resource_group_name = var.resource_group_name # Use variable for RG
 
   container {
     name   = var.container_name
-    image  = var.image_name                       # Make image configurable via variable
+    image  = var.image_name # Make image configurable via variable
     cpu    = var.cpu_core_number
     memory = var.memory_size
 
@@ -19,9 +19,9 @@ resource "azurerm_container_group" "aci" {
 
   # Add this block for image registry credentials if using a private registry
   image_registry_credential {
-    server   = var.registry_server                # Registry URL
-    username = var.registry_username              # Username for authentication
-    password = var.registry_password              # Password for authentication
+    server   = var.registry_server   # Registry URL
+    username = var.registry_username # Username for authentication
+    password = var.registry_password # Password for authentication
   }
 }
 
